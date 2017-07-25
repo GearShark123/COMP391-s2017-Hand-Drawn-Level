26faebea2e8d470d2e06d4cb5bfb8932b97b28d0
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2017-07-25 17:02:59 -0400

    Camera follower is added

--- Hand Drawn Level Prototype/Assets/Scripts/CameraFollower.cs
+++ Hand Drawn Level Prototype/Assets/Scripts/CameraFollower.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollower : MonoBehaviour {
+public class CameraFollower : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float dampFactor = 0.15f;
+    public Transform target;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 initialPosition;
+    private new Camera camera;
+
+    void Start()
+    {
+        camera = Camera.main;
+
+        initialPosition = camera.WorldToViewportPoint(target.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 diff = target.position - camera.ViewportToWorldPoint(initialPosition);
+            Vector3 destination = transform.position + diff;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampFactor);
+        }
+
+    }
 }
