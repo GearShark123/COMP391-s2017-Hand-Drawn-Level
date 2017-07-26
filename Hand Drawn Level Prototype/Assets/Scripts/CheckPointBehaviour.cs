cc05a16548330ec692df0b90f172c8e023b3bc51
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2017-07-26 14:14:53 -0400

    Checkpoint prefab is created

---
+++ Hand Drawn Level Prototype/Assets/Scripts/CheckPointBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointBehaviour : MonoBehaviour {
+
+    // Use this for initialization
+    private void Spawn(GameObject gameObject) {
+        gameObject.transform.position = transform.position;
+        gameObject.SetActive(true);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+    }
+}
