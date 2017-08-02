7f2d6a453e10e64db2f86369c0beca52f63c71d8
GearShark123 <lylespurrell@hotmail.com>
2017-08-02 08:32:24 -0400

    Added arm and related object to the character

---
+++ Hand Drawn Level Prototype/Assets/Scripts/WeaponPortalControls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPortalControls : MonoBehaviour {
+
+    public GameObject weapon;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            weapon.SetActive = true;
+        }
+        else
+        {
+            weapon.SetActive = false;
+        }
+    }
+}
