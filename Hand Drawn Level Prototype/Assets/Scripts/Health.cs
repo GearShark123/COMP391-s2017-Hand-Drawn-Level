0dcbe473638bb3506a136724ec49dedb35e8d2ca
GearShark123 <lylespurrell@hotmail.com>
2017-08-11 11:47:53 -0400

    Red Screen on Player Damage

--- Hand Drawn Level Prototype/Assets/Scripts/Health.cs
+++ Hand Drawn Level Prototype/Assets/Scripts/Health.cs
@@ -4,22 +4,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Health : MonoBehaviour {
+public class Health : MonoBehaviour
+{
 
-    public float healthPoints; 
+    public int healthLimit;              // 10
+    public float healthPoints;
+    public GameObject redScreen;
+    private GameObject playerCamera;
 
     private bool IsImmune { set; get; }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {        
+        playerCamera = GameObject.Find("Main Camera");
         IsImmune = false;
     }
 
-    public void DamageTaken(float damage, float timeToDie, Action onDie) {
+    public void DamageTaken(float damage, float timeToDie, Action onDie)
+    {
         if (IsImmune) return;
-         //print(healthPoints);
+        //print(healthPoints);
         healthPoints -= damage;
-         print(healthPoints);
+        for (int i = 0; i <= damage; i++)
+        {
+            Instantiate(redScreen, playerCamera.transform);
+        }
+        //Instantiate(redScreen, playerCamera.transform);
+        print(healthPoints);
         if (healthPoints <= 0f)
         {
             onDie();
