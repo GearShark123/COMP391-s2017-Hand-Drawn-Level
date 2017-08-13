a5d034d54325192aca3d39200fc0d60e035f1c12
GearShark123 <lylespurrell@hotmail.com>
2017-08-12 21:44:13 -0400

    Fixed Sword Collision

--- Hand Drawn Level Prototype/Assets/Scripts/Player/SwordBehaviour.cs
+++ Hand Drawn Level Prototype/Assets/Scripts/Player/SwordBehaviour.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwordBehaviour : MonoBehaviour {
+public class SwordBehaviour : MonoBehaviour
+{
 
     public float timeToDestroy = 5.0f;
     public float speed = 1.0f;
     public float damage;
     private bool hit;
+
+    private float time;
+    private float timeAdded = 0.1f;
+    private float timeLimit = 0.2f;
 
     // Use this for initialization
     void Start()
@@ -19,6 +24,7 @@ public class SwordBehaviour : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        time += timeAdded;
         if (hit == false)
         {
             Vector3 direction = transform.right.normalized;
@@ -26,23 +32,16 @@ public class SwordBehaviour : MonoBehaviour {
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        HandlePlayerCollision(collision);
-    }
-
-    //private void OnTriggerExit2D(Collider2D collision)
-    //{
-    //    HandlePlayerCollision(collision);
-    //}
-
-    //private void OnTriggerStay2D(Collider2D collision)
-    //{
-    //    HandlePlayerCollision(collision);
-    //}
-
-    private void HandlePlayerCollision(Collider2D collision)
-    {
-        hit = true;                
+    void OnCollisionEnter2D(Collision2D collision)
+    {        
+        if (collision.collider.tag != "Player" && collision.collider.tag != "IgnorePortal")
+        {
+            //print(collision.collider);
+            hit = true;
+        }
+        if (time >= timeLimit)
+        {
+            hit = true;
+        }
     }
 }
