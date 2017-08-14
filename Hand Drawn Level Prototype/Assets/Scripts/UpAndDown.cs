f576091509eb8062155dfc552beceda13492acdb
Unknown <kenseverus@gmail.com>
2017-08-14 12:50:12 -0400

    Horizontal Moving Object
    Added BackAndForth.cs
    Added UpAndDown.cs

---
+++ Hand Drawn Level Prototype/Assets/Scripts/UpAndDown.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpAndDown : MonoBehaviour {
+
+    public float delta = 8.0f;  // Amount to move left and right from the start point
+    public float speed = 4.0f;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = startPos;
+        v.y += delta * Mathf.Cos(Time.time * speed);
+        transform.position = v;
+    }
+}
