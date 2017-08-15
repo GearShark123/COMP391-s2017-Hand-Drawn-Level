b30826d32be1497ff22596cf385f0e987d781d82
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2017-08-15 10:27:31 -0400

    Golem is being able to kill

--- Hand Drawn Level Prototype/Assets/Scripts/BossBehaviour.cs
+++ Hand Drawn Level Prototype/Assets/Scripts/BossBehaviour.cs
@@ -8,25 +8,27 @@ public class BossBehaviour : MonoBehaviour {
     public float minDistanceToPlayer = 2.0f;
     public float maxDistanceToPlayer = 25.0f;
     public float maxDistanceFromDungeon = 30;
+    public float damage = 4.0f;
 
     private Animator animator;
     private Vector3 initialPosition;
 
-    private CheckPointBehaviour checkpoint;
+    private PlayerControls currentPlayer;
     private GameManagerBehaviour gameManager;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         animator = GetComponent<Animator>();
-        checkpoint = GameObject.FindObjectOfType<CheckPointBehaviour>();
+        currentPlayer = GameObject.FindObjectOfType<PlayerControls>();
         initialPosition = transform.position;
         gameManager = GameObject.FindObjectOfType<GameManagerBehaviour>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (checkpoint.currentPlayer != null) {
-            float directionX = checkpoint.currentPlayer.transform.position.x - transform.position.x;
+        if (currentPlayer != null)
+        {
+            float directionX = currentPlayer.transform.position.x - transform.position.x;
             if (directionX < 0 && (transform.position - initialPosition).magnitude > maxDistanceFromDungeon)
             {
                 animator.SetBool("IsWalking", false);
@@ -37,8 +39,11 @@ public class BossBehaviour : MonoBehaviour {
                 animator.SetBool("IsWalking", true);
                 transform.position += Vector3.right * (directionX) * speed * Time.deltaTime;
             }
-            else {
+            else
+            {
                 animator.SetBool("IsWalking", false);
+                if (Mathf.Abs(directionX) < minDistanceToPlayer)
+                    animator.SetTrigger("Punch");
             }
             if (directionX > 0)
             {
@@ -51,7 +56,16 @@ public class BossBehaviour : MonoBehaviour {
                 transform.localScale = new Vector3(Mathf.Abs(scale.x), scale.y, scale.z);
             }
         }
+        else {
+            currentPlayer = GameObject.FindObjectOfType<PlayerControls>();
+        }
 	}
+    public void Punch() {
+        if (currentPlayer && currentPlayer.transform.position.y<transform.position.y) {
+            Health health = currentPlayer.GetComponent<Health>();
+            health.DamageTaken(damage, 1.0f, () => Destroy(health.gameObject));
+        }
+    }
 
     private void OnDestroy()
     {
